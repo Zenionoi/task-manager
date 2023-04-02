223c74e74d8720fe13bddf11461d25a9f9237f7d
Zenon <zenion@ukr.net>
2023-04-02 22:37:43 +0300

    Added models and initial migration file

--- apps/manager/apps.py
+++ apps/manager/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class ManagerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "manager"
+    name = 'apps.manager'
+    label = 'apps_manager'
--- apps/manager/models.py
+++ apps/manager/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}({self.position})"
+
+    def get_absolute_url(self):
+        return reverse("manager:worker-detail", kwargs={"pk": self.pk})
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    deadline = models.DateField(null=True)
+    is_completed = models.BooleanField(default=False)
+
+    class PriorityChoices(models.TextChoices):
+        URGENT = "U", _("Urgent")
+        IMPORTANT = "I", _("Important")
+        MINOR = "M", _("Minor")
+        DEFERRED = "D", _("Deferred")
+
+    priority = models.CharField(
+        max_length=1,
+        choices=PriorityChoices.choices,
+        default=PriorityChoices.MINOR,
+    )
+
+    tasktype = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    def __str__(self):
+        return f"{self.name} with {self.priority} priority"
