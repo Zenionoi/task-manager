2f582ab49e2fdc9b750a38e51c58bb64c826a7bf
Zenon <zenion@ukr.net>
2023-04-02 23:25:50 +0300

    Added SetUp instructions to README.md and fixed minor bugs

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-AUTH_USER_MODEL = "manager.Worker"
+AUTH_USER_MODEL = "apps_manager.Worker"
 
 LANGUAGE_CODE = 'en-us'
 
