aa6143f3232c6942c6a030a63b2865d4f6102d95
Zenon <zenion@ukr.net>
2023-05-24 01:04:41 +0300

    Added test configuration and fixed minor bugs

--- apps/manager/views.py
+++ apps/manager/views.py
@@ -189,7 +189,7 @@ class PositionListView(LoginRequiredMixin, generic.ListView):
     model = TaskType
     context_object_name = "position_list"
     template_name = "manager/position_list.html"
-    paginate_by = 5
+    paginate_by = 7
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PositionListView, self).get_context_data(**kwargs)
